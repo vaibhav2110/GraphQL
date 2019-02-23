4d427a16d9d8e43142cbb93312db46ab933aef9f
Vaibhav <vaibhpraky@gmail.com>
2019-02-23 18:26:44 +0530

    Initial Setup

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphQlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/graphql', graphQlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return [];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
+
+app.listen(3000);
