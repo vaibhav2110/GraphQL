1ba516a6ba9a92e51a7a0d130797e2e38ee96ff8
Vaibhav <vaibhpraky@gmail.com>
2019-03-06 19:37:31 +0530

    Event Booking

--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -1,11 +1,113 @@
-import React from 'react';
+import React from "react";
+import AuthContext from "../context/auth-context";
+import Spinner from "../components/Spinner/Spinner";
+import BookingList from "../components/Bookings/BookingList/BookingList";
 
-class BookingsPage extends React.Component{
-    render(){
-        return (
-            <div>Bookings</div>
-        );
-    }
+class BookingsPage extends React.Component {
+  state = {
+    isLoading: false,
+    bookings: []
+  };
+
+  static contextType = AuthContext;
+
+  componentDidMount() {
+    this.fetchBookings();
+  }
+
+  fetchBookings = () => {
+    this.setState({ isLoading: true });
+    const requestBody = {
+      query: `
+                query {
+                    bookings{
+                        _id
+                        createdAt
+                        event{
+                            _id
+                            title
+                            date
+                        }
+                    }
+                }
+            `
+    };
+
+    fetch("http://localhost:8000/graphql", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + this.context.token
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then(resData => {
+        const bookings = resData.data.bookings;
+        this.setState({ bookings: bookings, isLoading: false });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ isLoading: false });
+      });
+  };
+
+  deleteBookingHandler = bookingId => {
+    this.setState({ isLoading: true });
+    const requestBody = {
+      query: `
+                mutation {
+                    cancelBooking(bookingId: "${bookingId}") {
+                        _id
+                        title
+                    }
+                }
+            `
+    };
+
+    fetch("http://localhost:8000/graphql", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + this.context.token
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then(resData => {
+        this.setState(prevState => {
+            const updatedSBookings = prevState.bookings.filter(booking => {
+                return booking._id !== bookingId;
+            });
+            return { bookings: updatedSBookings, isLoading: false };
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ isLoading: false });
+      });
+  }
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.isLoading ? (
+          <Spinner />
+        ) : (
+          <BookingList onDelete={this.deleteBookingHandler} bookings={this.state.bookings}/>
+        )}
+      </React.Fragment>
+    );
+  }
 }
 
 export default BookingsPage;
--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,8 +1,17 @@
+const DataLoader = require('dataloader');
 const Event = require('../../models/event');
 const User = require('../../models/user');
 const {
     dateToString
 } = require('../../helpers/date');
+
+const eventLoader = new DataLoader((eventIds) => {
+    return events(eventIds);
+});
+
+const userLoader = new DataLoader((userIds) => {
+    return User.find({_id: {$in: userIds}});
+})
 
 const transformEvent = event => {
     return {
@@ -30,11 +39,11 @@ const events = async eventIds => {
 
 const user = async userId => {
     try {
-        const user = await User.findById(userId)
+        const user = await userLoader.load(userId.toString());
         return {
             ...user._doc,
             _id: user.id,
-            createdEvents: events.bind(this, user._doc.createdEvents)
+            createdEvents: eventLoader.load.bind(this, user._doc.createdEvents)
         };
     } catch (err) {
         throw err;
@@ -43,8 +52,8 @@ const user = async userId => {
 
 const singleEvent = async eventId => {
     try {
-        const event = await Event.findById(eventId);
-        return transformEvent(event);
+        const event = await eventLoader.load(eventId.toString());
+        return event;
     } catch (err) {
         throw err;
     }
